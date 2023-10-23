5efcbb7dd434ea61817bb57f5afb96f5ff38a971
田中　修平 <akashuhei@outlook.jp>
2023-10-23 18:43:21 +0900

    NewServlet 初期

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -14,16 +14,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class indexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class indexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public indexServlet() {
+    public NewServlet() {
         super();
     }
 
@@ -32,10 +32,18 @@ public class indexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks",Task.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+        Task t = new Task();
+        String content = "hello";
+        t.setContent(content);
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
 
+        em.persist(t);
+        em.getTransaction().commit();
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
         em.close();
     }
 
